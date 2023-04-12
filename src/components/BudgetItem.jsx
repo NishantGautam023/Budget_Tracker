95ceed1ffc011984f3f92ca0c276cf80f60cee1b
nishant <nishant.gautam023@outlook.com>
2023-04-12 15:02:17 -0400

    Added Budget Card

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -9,7 +9,7 @@ export default function BudgetItem({ budget }) {
   const spentMoney = calculateBudgetSpent(id);
   return (
     <>
-      <div className="budget">
+      <div className="budget" style={{ "--accent": color }}>
         <div className="progess-text">
           <h3>{name}</h3>
           <p>{formatCurrency(amount)} Budgeted</p>
